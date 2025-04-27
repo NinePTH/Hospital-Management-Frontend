ebadaf163d9b3a5edaef1ca968bd3547bcf5e70a
Patihan Prasongphol <patihan70@gmail.com>
2025-04-28 03:53:36 +0700

    Fix: import error

--- src/components/utils/ProtectedRoute.tsx
+++ src/components/utils/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../hooks/UseAuth";
+import { useAuth } from "../../hooks/UseAuth";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const { isAuthenticated } = useAuth();
