b28a31d29d9bebc5d73bcfbb9fed86e2f073ebef
Patihan Prasongphol <patihan70@gmail.com>
2025-04-30 03:51:05 +0700

    Complete: Add Patient page

---
+++ src/components/patientManagement/AddPatient.tsx
@@ -0,0 +1,17 @@
+import AddPatientForm from "./AddPatientForm";
+
+const AddPatient = () => {
+  return (
+    <>
+      <div className="bg-white shadow-soft min-h-[82.5dvh] flex flex-col items-center justify-start gap-8 py-14 px-14 rounded-lg">
+        <header className="text-center">
+          <h1 className="font-playfair text-4xl pb-2">New Patient</h1>
+          <h2 className="text-xl">Siam Hospital</h2>
+        </header>
+        <AddPatientForm />
+      </div>
+    </>
+  );
+};
+
+export default AddPatient;
--- src/pages/PatientManagement.tsx
+++ src/pages/PatientManagement.tsx
@@ -1,12 +1,42 @@
+import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import AddPatient from "../components/patientManagement/AddPatient";
 import Sidebar from "../components/Sidebar";
 
 const PatientManagement: React.FC = (): JSX.Element => {
+  const location = useLocation();
+
+  const components: Record<string, { path: string; component: JSX.Element }[]> =
+    {
+      "patients-management": [
+        { path: "add-patient", component: <AddPatient /> },
+        { path: "edit-patient", component: <AddPatient /> },
+        { path: "add-patient-apppointment", component: <AddPatient /> },
+        { path: "add-patient-medical-history", component: <AddPatient /> },
+        { path: "search-patient", component: <AddPatient /> },
+      ],
+      "employee-management": [
+        { path: "add-employee", component: <AddPatient /> },
+        { path: "edit-employee", component: <AddPatient /> },
+        { path: "search-employee", component: <AddPatient /> },
+      ],
+    };
+
+  const currentPath = location.pathname.split("/")[1];
+  console.log(currentPath);
+
+  const renderComponent = components[currentPath].find(
+    (component) => component.path === location.pathname.split("/")[2]
+  )?.component;
+
   return (
-    <>
+    <div className="bg-[url('/patientManagementSystem/patientManagementBG.webp')] bg-cover">
       <Navbar />
-      <Sidebar />
-    </>
+      <div className="flex flex-row justify-start items-start w-dvw min-h-dvh h-full">
+        <Sidebar />
+        <div className="w-full mt-auto pt-28 pb-8 md:px-14 lg:px-28 xl:px-56">{renderComponent}</div>
+      </div>
+    </div>
   );
 };
 
